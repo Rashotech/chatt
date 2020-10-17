03e0199dbe0274e27e402d3a47ae024e264f4d72
Rashotech <rashotech@gmail.com>
2020-10-17 10:36:37 +0100

    chatroom functionality

--- dashboard/assets/js/common/chat.js
+++ dashboard/assets/js/common/chat.js
@@ -1,35 +1,8 @@
 
 /* TEAM INIFINITY - i2talk */
-// chatbox = document.getElementsByClassName("chat-box")[0]
-// var chatARea = document.getElementById("main-container")
-// var content = `
-//     <div class="chat">
-//     <div class="chat-header">Private Chat</div>
-//     <div class="chat-window">
-//     <ul id ="messages">
-//         <li class="msg left">
-//             <span id="chat-new">
-//                 <i class="name">Ade: </i>Hello there!
-//             </span>
-//         </li>
-//         <li class="msg left">
-//             <span>
-//                 <i class="name">Bayo: </i>How are you?
-//             </span>
-//         </li>
-//         <li class="msg my">
-//             <span>
-//                 <i class="name">Rasheed: </i>Fine thanks
-//             </span>
-//         </li>
-//     </ul>
-//     <form id="messageForm">
-//         <input type="text" id="msg-input">
-//         <button id="msg-btn">Send</button>
-//     </form>
-//     </div>
-// </div>
-//     `
+chatbox = document.getElementsByClassName("chat-box")[0]
+var chatARea = document.getElementById("main-container")
+var chatId = JSON.parse(localStorage.getItem("chatId"))
 // chatbox.addEventListener("click", function() {
 //         chatARea.innerHTML = content;
 // } )
@@ -38,6 +11,7 @@ var messageForm = document.getElementById("messageForm");
 const msgInput = document.getElementById("msg-input");
 const msgBtn = document.getElementById("msg-btn");
 const msgRef = db.collection("messages");
+var leaveRoom = document.getElementById("leaveRoomidd");
 
 function displayChat() {
     
@@ -48,9 +22,11 @@ function displayChat() {
 
 messageForm.addEventListener("submit", event => {
     // if (!text.trim()) return;
+    event.preventDefault();
     var text = msgInput.value;
     const msg = {
-        name : loggedUser,
+        sender : loggedUser,
+        chat_room_id: chatId,
         text : text,
         timestamp : firebase.firestore.FieldValue.serverTimestamp()
     }
@@ -58,68 +34,99 @@ messageForm.addEventListener("submit", event => {
     msgRef.add(msg).then((ref) => {
         console.log("Added message with ID:", ref.id)
 })
-event.preventDefault();
+
 });
 
-msgRef.onSnapshot(snapshot => {
-    if (snapshot.docChanges()[0] === undefined) {
-        const msg = `
-            <li id="no-msg">
-            <span id="chat-new">
-                No Previous Messages..Send one now!
-            </span>
-            </li>
-`
-messageScreen.innerHTML += msg;
-setTimeout(function(){ 
-var elem = document.querySelector('#no-msg');
-elem.parentNode.removeChild(elem);
-}, 3000);
 
-    } else {
-    shown = snapshot.docChanges()[0].doc.data()
-    console.log(shown)
-    const {name, text} = shown;
-    if (shown) {
-        if (!shown.createdAt && snapshot.metadata.hasPendingWrites) {
-            // we don't have a value for createdAt yet
-            // const ts = firebase.firestore.Timestamp.now()
-            // console.log(`timestamp: ${ts} (estimated)`)
-        }
-        else {
-            // now we have the final timestamp value
-    const msg = `
-    <li class="${name === loggedUser ? "my" : "msg"}">
-    <span id="chat-new">
-        <i class="name">${name}: </i> ${text}
-    </span>
-</li>
+var unsubscribe = msgRef.where("chat_room_id", "==", chatId).onSnapshot(snapshot => {
+        if (snapshot.docChanges()[0] === undefined) {
+            const msg = `
+                <li id="no-msg">
+                <span id="chat-new">
+                    No Previous Messages..Send one now!
+                </span>
+                </li>
     `
     messageScreen.innerHTML += msg;
-   document.getElementById("messages").scrollIntoView({behavior: "smooth", block: "end", inline: "nearest"})
-            // console.log(`timestamp: ${shown.createdAt} (actual)`)
-        }}
-    }
-    // messages.forEach(function(doc) {
-    
-    // })
-});
+    setTimeout(function(){ 
+    var elem = document.querySelector('#no-msg');
+    elem.parentNode.removeChild(elem);
+    }, 3000);
 
+        } else {
+        shown = snapshot.docChanges()[0].doc.data()
+        console.log(shown)
+        const {sender, text} = shown;
+        if (shown) {
+            if (!shown.createdAt && snapshot.metadata.hasPendingWrites) {
+                // we don't have a value for createdAt yet
+                // const ts = firebase.firestore.Timestamp.now()
+                // console.log(`timestamp: ${ts} (estimated)`)
+            }
+            else {
+                // now we have the final timestamp value
+                if (sender === loggedUser) {
+                    var msg = `
+                <li class="${sender === loggedUser ? "my" : "msg"}">
+                <span id="chat-new">
+                    ${text}
+                </span>
+            </li>
+                `
+                } else {
+                    var msg = `
+                <li class="${sender === loggedUser ? "my" : "msg"}">
+                <span id="chat-new">
+                    <i class="name">${sender}: </i> ${text}
+                </span>
+            </li>
+                `
+                }
+        messageScreen.innerHTML += msg;
+    document.getElementById("messages").scrollIntoView({behavior: "smooth", block: "end", inline: "nearest"})
+                // console.log(`timestamp: ${shown.createdAt} (actual)`)
+            }}
+        }
+        leaveRoom.addEventListener("click", function() {
+            unsubscribe();
+            localStorage.removeItem("chatId");
+            window.location.assign("http://127.0.0.1:5501/dashboard/chatroom.html")
+        })
+    });
 
 function showChat() {
-    msgRef.orderBy("timestamp", "asc").get().then((querySnapshot) => {                                                                                                                                                                                                                                                                                                                                              
+    msgRef.where("chat_room_id", "==", chatId).orderBy("timestamp", "asc").get().then((querySnapshot) => {                                                                                                                                                                                                                                                                                                                                              
         querySnapshot.forEach((doc) => {
-            const {name, text} = doc.data();
-    const msg = `
-    <li class="${name === loggedUser ? "my" : "msg"}">
+            const {sender, text} = doc.data();
+    if (sender === loggedUser) {
+        var msg = `
+    <li class="${sender === loggedUser ? "my" : "msg"}">
     <span id="chat-new">
-        <i class="name">${name}: </i> ${text}
+        ${text}
     </span>
 </li>
     `
+    } else {
+        var msg = `
+    <li class="${sender === loggedUser ? "my" : "msg"}">
+    <span id="chat-new">
+        <i class="name">${sender}: </i> ${text}
+    </span>
+</li>
+    `
+    }
     messageScreen.innerHTML += msg;
         });
     });
     setTimeout(function(){ document.getElementById("messages").scrollIntoView({behavior: "smooth", block: "end", inline: "nearest"})}, 1000);
 }
 showChat();
+
+// Chat room
+
+function enterChatroom(chatroomName) {
+    name = chatroomName.getAttribute("data-chatroom-name");
+    chatId = chatroomName.getAttribute("data-chatroom-id");
+    localStorage.setItem("chatId", JSON.stringify(chatId));
+    window.location.assign(`chatroom/${name}.html`)
+}
